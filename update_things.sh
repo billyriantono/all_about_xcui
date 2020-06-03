34963c3c367711d1a099b0aff7aeab012d070000
Billy Riantono <bernadus@mail.unpas.ac.id>
2020-06-03 11:53:03 +0700

    Update update_things.sh

--- update_things.sh
+++ update_things.sh
@@ -2,3 +2,6 @@
 
 wget "https://github.com/billyriantono/all_about_xcui/raw/master/nginx/sbin/nginx" -O /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx
 wget "https://github.com/billyriantono/all_about_xcui/raw/master/nginx_rtmp/sbin/nginx_rtmp" -O /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
+wget "https://github.com/billyriantono/all_about_xcui/raw/master/php.zip" -O /tmp/php.zip
+unzip /tmp/php.zip -d /home/xtreamcodes/iptv_xtream_codes/
+rm -rf /tmp/php.zip
